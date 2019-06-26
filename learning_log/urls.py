a75d8e5d5489c3d9f465741cce69028994165668
dizhu <dizhu210@gmail.com>
2019-06-25 21:15:20 -0400

    set users app

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import urls as users_url
 from learning_logs import urls as learning_logs_url
 
 urlpatterns = [
     path('', include((learning_logs_url,'learning_logs'), namespace='learning_logs')),
+    path('', include((users_url,'users'), namespace='users')),
     path('admin/', admin.site.urls),
 ]
