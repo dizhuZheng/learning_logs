923de0b7704de966a41275e08c6ccd4928ddcf6a
dizhu <dizhu210@gmail.com>
2019-06-26 20:14:51 -0400

    register works

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(template_name='users/login.html'),
         name='login'),
     #logout page
-    url(r'^logout/$', views.logout_view, name='logout')
+    url(r'^logout/$', views.logout_view, name='logout'),
+
+    #registration page
+    url(r'^register/$', views.register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseRedirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -8,3 +10,22 @@ def logout_view(request):
     """log the user out."""
     logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
+def register(request):
+    """register new user"""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        #process completed form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #log the user in and then redirect to home page
+            authenticated_user = authenticate(username=new_user.username,
+            password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
